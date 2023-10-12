2e7273743092559aa95196c8ec8e2970982db8fb
Julien Montavit <j.montavit@gmail.com>
2023-10-12 23:13:42 +0200

    feat : changed cursorLineNr color

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -1,1 +1,13 @@
+U = require "catppuccin.utils.colors"
+
+require("catppuccin").setup {
+    highlight_overrides = {
+        all = function(colors)
+            return {
+                CursorLineNr = { fg = colors.lavender, bg = U.darken(colors.overlay2, 0.2, colors.base) },
+            }
+        end,
+    },
+}
+
 vim.cmd.colorscheme "catppuccin"
